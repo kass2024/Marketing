1831b94a664dad52ccf290709cca9124546e6229
methode <ujeanmethode@gmail.com>
2026-03-04 22:20:01 +0300

    ads manager

--- app/Models/AdAccount.php
+++ app/Models/AdAccount.php
@@ -9,14 +9,32 @@ class AdAccount extends Model
 {
     use HasFactory;
 
+    protected $table = 'ad_accounts';
+
+    /**
+     * Mass assignable fields
+     */
     protected $fillable = [
         'client_id',
+        'meta_id',
         'ad_account_id',
         'name',
         'currency',
         'account_status',
     ];
 
+    /**
+     * Attribute casting
+     */
+    protected $casts = [
+        'client_id' => 'integer',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    /**
+     * Relationship: AdAccount belongs to Client
+     */
     public function client()
     {
         return $this->belongsTo(Client::class);

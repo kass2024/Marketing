a35d252d28062f9d9892660f14d0c563b8c3857b
methode <ujeanmethode@gmail.com>
2026-03-24 07:31:43 +0300

    adset

--- app/Http/Controllers/Admin/AdController.php
+++ app/Http/Controllers/Admin/AdController.php
@@ -864,7 +864,7 @@ public function sync(Ad $ad): RedirectResponse
 
             'impressions' => $impressions,
             'clicks' => $clicks,
-            'spend' => max($ad->spend, $spend),
+            'spend' => $spend,
             'ctr' => $ctr
 
         ]);
